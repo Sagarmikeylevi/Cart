181820283814e65bcb2a243f20a73165f4a75972
unknown <sagarmikeylevi@gmail.com>
2023-02-26 12:51:27 +0530

    Modify the increase and decrease part

--- src/Cart.js
+++ src/Cart.js
@@ -8,21 +8,21 @@ class Cart extends React.Component {
                 {
                     price: 9999,
                     title: 'Mobile Phone',
-                    qty: 1,
+                    qty: 0,
                     img: '',
                     id: 1
                 },
                 {
                     price: 999,
                     title: 'Watch',
-                    qty: 5,
+                    qty: 0,
                     img: '',
                     id: 2
                 },
                 {
                     price: 99,
                     title: 'Balls',
-                    qty: 2,
+                    qty: 0,
                     img: '',
                     id: 3
                 }
@@ -31,6 +31,31 @@ class Cart extends React.Component {
         }
 
         // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+
+    handleIncreaseQuantity = (product) =>{
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products
+        });
+
+    }
+    handleDecreaseQuantity = (product) =>{
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        if(products[index].qty == 0){
+            return;
+        }
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        });
+
     }
     render() {
         const { products } = this.state;
@@ -41,12 +66,15 @@ class Cart extends React.Component {
                         <CartItem
                             product={product}
                             key={product.id}
+                            onIncreaseQuantity={this.handleIncreaseQuantity}
+                            onDecreaseQuantity={this.handleDecreaseQuantity}
                         />
                     )
                 })}
             </div>
         );
     }
+
 }
 
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,35 +1,10 @@
 import React from 'react';
 class CartItem extends React.Component {
-    
-    
-    increaseQuantity = () =>{
-        // setState form-1
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // });
-
-        // setState form-2
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty + 1
-            }
-        });
-    }
-    decreaseQuantity = () =>{
-        const {qty} = this.state;
-        if(qty == 0) return;
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-    }
-
-    
     render() {
-        console.log('render');
-        console.log('this.props' , this.props.product);
+        // console.log('render');
+        // console.log('this.props' , this.props.product);
         const { price, title, qty } = this.props.product;
+        const { product , onIncreaseQuantity , onDecreaseQuantity } = this.props;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -44,13 +19,13 @@ class CartItem extends React.Component {
                             alt='increase'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/128/3303/3303893.png'
-                            onClick={this.increaseQuantity}
+                            onClick={() => onIncreaseQuantity(product)}
                         />
                         <img
                             alt='decrease'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/128/992/992683.png'
-                            onClick={this.decreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
                         />
                         <img
                             alt='delete'
