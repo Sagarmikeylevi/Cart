ef5716dbee6a6ba24b28cc5ca0aa8e8e76fb873a
unknown <sagarmikeylevi@gmail.com>
2023-03-01 12:02:10 +0530

    Modify the componentDidMount part and add onSnapshot

--- src/App.js
+++ src/App.js
@@ -15,11 +15,32 @@ class App extends React.Component {
   }
 
   componentDidMount () {
+    // firebase
+    //   .firestore()
+    //   .collection('products')
+    //   .get()
+    //   .then((snapshot) =>{
+    //     console.log(snapshot);
+
+    //     snapshot.docs.map((doc) =>{
+    //       console.log(doc.data());
+    //     });
+
+    //     const products = snapshot.docs.map((doc) =>{
+    //       const data = doc.data();
+    //       data['id'] = doc.id;
+    //       return data;
+    //     });
+
+    //     this.setState({
+    //       products,
+    //       loading: false
+    //     });
+    //   });
     firebase
       .firestore()
       .collection('products')
-      .get()
-      .then((snapshot) =>{
+      .onSnapshot((snapshot) =>{
         console.log(snapshot);
 
         snapshot.docs.map((doc) =>{
